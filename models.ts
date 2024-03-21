9cb63daf97f92184d3f06e6b71a6c186c99b2b7d
Kirra Hyde <kirra.hyde@gmail.com>
2024-03-21 00:42:22 -0400

    models

--- models.ts
+++ models.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { randPlayer } from "./utilties.js";
+import { randPlayer, changePlayer } from "./utilties.js";
 
 const BASE_URL = "https://deckofcardsapi.com/api/deck";
 let currGame: Game;
@@ -13,36 +13,37 @@ type tCardData = {
   suit: string;
 };
 
-class Card {
-  suit: string;
-  rank: string;
-  flipped: boolean;
-  img: string;
 
-  constructor(suit: string, rank: string, img: string) {
-    this.suit = suit;
-    this.rank = rank;
-    this.img = img;
+class Card {
+  code: string;
+  value: string;
+  flipped: boolean;
+  image: string;
+
+  constructor(value: string, image: string, code: string) {
+    this.code = code;
+    this.value = value;
+    this.image = image;
     this.flipped = false;
   }
 }
 
 class Game {
   deckID: string;
-  numCards: number;
   players: Player[];
   currPlayer: Player;
+  topDiscard: Card | null;
 
   constructor(deckID: string, players: Player[]) {
     this.deckID = deckID;
-    this.numCards = 52;
     this.players = players;
     this.currPlayer = randPlayer(players);
+    this.topDiscard = null;
   }
 
-  static async startGame() {
+  static async startGame(): Promise<void> {
     const resp = await axios.get(`${BASE_URL}/new/shuffle`);
-    const deckID: string = resp.data.deckID;
+    const deckID = resp.data.deckID as string;
 
     const mainName = "You";  // Will eventually get value from form. Which means, dictate that it is a string?  Validation check for not billy, etc.
     const names = [mainName, "Billy", "Bobby", "Buddy"];
@@ -51,32 +52,101 @@ class Game {
     currGame = new Game(deckID, players);
   }
 
-  async dealGame() {
+  async dealGame(): Promise<void> {
     for (let player of this.players) {
       const resp = await axios.get(
         `${BASE_URL}/${this.deckID}/draw`,
         { params: { count: CARDS_PER_HAND } }
       );
 
-      const cardData: tCardData[] = resp.data.cards;
+      const cardData = resp.data.cards as tCardData[];
       player.cards =
-        cardData.map(({ suit, value, image }) => new Card(suit, value, image));
+        cardData.map(({ value, image, code }) => new Card(value, image, code));
     }
-    this.numCards -= (CARDS_PER_HAND * this.players.length);
+    const resp = await axios.get(`${BASE_URL}/${this.deckID}/draw`);
+
+    const cardData = resp.data.cards[0] as tCardData;
+    const { value, image, code } = cardData;
+    this.topDiscard = new Card(value, image, code);
+  }
+
+  switchTurn(): void {
+    this.currPlayer = changePlayer(this.currPlayer, this.players);
+  }
+
+  async reshuffle(): Promise<void> {
+    if (this.topDiscard instanceof Card) {
+      await axios.get(
+        `${BASE_URL}/${this.deckID}/pile/discard/add`,
+        { params: { cards: this.topDiscard.code } }
+      );
+    }
+    await axios.get(`${BASE_URL}/${this.deckID}/pile/discard/return`);
+    await axios.get(
+      `${BASE_URL}/${this.deckID}/shuffle`,
+      { params: { remaining: true } }
+    );
   }
 }
 
 class Player {
   cards: Card[];
   name: string;
+  drawnCard: Card | null;
+
   constructor(name: string) {
     this.cards = [];
     this.name = name;
+    this.drawnCard = null;
   }
 
-  firstFlip(cardInd1: number, cardInd2: number) {
-    this.cards[cardInd1].flipped = true;
-    this.cards[cardInd2].flipped = true;
+  flipCard(cardInd: number): void {
+    this.cards[cardInd].flipped = true;
+  }
+
+  async drawFromDeck(): Promise<void> {
+
+    const resp = await axios.get(`${BASE_URL}/${currGame.deckID}/draw`);
+
+    const success = resp.data.success as boolean;
+    if (!success) {
+      await currGame.reshuffle();
+      this.drawFromDeck();
+    }
+
+    const cardData = resp.data.cards[0] as tCardData;
+    const { value, image, code } = cardData;
+    this.drawnCard = new Card(value, image, code);
+  }
+
+  drawFromDiscards(): void {
+    this.drawnCard = currGame.topDiscard as Card;
+    currGame.topDiscard = null;
+  }
+
+  async discardDrawnCard(): Promise<void> {
+    if (currGame.topDiscard instanceof Card) {
+      await axios.get(
+        `${BASE_URL}/${currGame.deckID}/pile/discard/add`,
+        { params: { cards: currGame.topDiscard.code } }
+      );
+    }
+
+    currGame.topDiscard = this.drawnCard as Card;
+    this.drawnCard = null;
+  }
+
+  async takeDrawnCard(cardInd: number): Promise<void> {
+    if (currGame.topDiscard instanceof Card) {
+      await axios.get(
+        `${BASE_URL}/${currGame.deckID}/pile/discard/add`,
+        { params: { cards: currGame.topDiscard.code } }
+      );
+    }
+    currGame.topDiscard = this.cards[cardInd];
+    this.cards[cardInd] = this.drawnCard as Card;
+    this.cards[cardInd].flipped = true;
+    this.drawnCard = null;
   }
 }
 
--- utilties.ts
+++ utilties.ts
@@ -1,8 +1,17 @@
 import { Player } from "./models.js";
 
-function randPlayer(players: Player[]) {
+function randPlayer(players: Player[]): Player {
   const playerNum = Math.floor(Math.random() * players.length);
   return players[playerNum];
 }
 
-export { randPlayer };
+function changePlayer(player: Player, players: Player[]): Player {
+  const currPlayerInd = players.findIndex(p => p.name === player.name);
+  if ((currPlayerInd + 1) < players.length) {
+    return players[currPlayerInd + 1];
+  } else {
+    return players[0];
+  }
+}
+
+export { randPlayer, changePlayer };
